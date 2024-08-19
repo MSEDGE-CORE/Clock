6de91ff9341b81f19777fbdd7a867b41215e6061
MSEDGE <EdgeXiaoLe@outlook.com>
2024-08-19 20:47:14 +0800

    改进布局响应

--- StopWatch.xaml.cs
+++ StopWatch.xaml.cs
@@ -224,38 +224,10 @@ namespace Clock
             catch { }
         }
 
-        private async void StopWatchFlagButton_Click(object sender, RoutedEventArgs e)
+        int Minute = 0, Second = 0, MSecond = 0;
+        string s0 = "", sMinute = "00", sSecond = "00", sMSecond = "00";
+        private void GetHMS(long NowTicks = 0)
         {
-            long NowTicks = (DateTime.Now.Ticks / 10000 - (Application.Current as App).StopWatch_StartTick);
-            if (NowTicks <= 0)
-            {
-                NowTicks = 0;
-                return;
-            }
-            int Minute = (int)((NowTicks) / 1000 / 60);
-            int Second = (int)((NowTicks % (1000 * 60)) / 1000);
-            int MSecond = (int)((NowTicks % (1000)) / 10);
-            string s0 = "";
-            if (Minute / 10 == 0)
-                s0 = "0";
-            else
-                s0 = "";
-            string sMinute = s0 + Minute.ToString();
-            if (Second / 10 == 0)
-                s0 = "0";
-            else
-                s0 = "";
-            string sSecond = s0 + Second.ToString();
-            if (MSecond / 10 == 0)
-                s0 = "0";
-            else
-                s0 = "";
-            string sMSecond = s0 + MSecond.ToString();
-            string Total = sMinute + ":" + sSecond + "." + sMSecond;
-            //
-            long NowTicksTmp = NowTicks;
-            NowTicks = (NowTicksTmp - (Application.Current as App).StopWatch_LastFlagTick);
-            (Application.Current as App).StopWatch_LastFlagTick = NowTicksTmp;
             if (NowTicks <= 0)
             {
                 NowTicks = 0;
@@ -279,65 +251,39 @@ namespace Clock
             else
                 s0 = "";
             sMSecond = s0 + MSecond.ToString();
+        }
+
+        private async void StopWatchFlagButton_Click(object sender, RoutedEventArgs e)
+        {
+            long NowTicks = (DateTime.Now.Ticks / 10000 - (Application.Current as App).StopWatch_StartTick);
+            GetHMS(NowTicks);
+            string Total = sMinute + ":" + sSecond + "." + sMSecond; 
+
+            long NowTicksTmp = NowTicks;
+            NowTicks = (NowTicksTmp - (Application.Current as App).StopWatch_LastFlagTick);
+            (Application.Current as App).StopWatch_LastFlagTick = NowTicksTmp;
+            GetHMS(NowTicks);
             string Plus = sMinute + ":" + sSecond + "." + sMSecond;
-            //
+
             (Application.Current as App).SwFlagList.Insert(0, new SwFlag_List { Num = ((Application.Current as App).SwFlagList.Count() + 1).ToString(), Total = Total, Plus = Plus });
 
             Page_SizeChanged();
             FileStopWatch();
 
-            Windows.Storage.StorageFolder StorageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-            Windows.Storage.StorageFile FileCount = await StorageFolder.CreateFileAsync("StopWatchFlags\\Count.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
-            Windows.Storage.StorageFile FileFlags = await StorageFolder.CreateFileAsync("StopWatchFlags\\Flag" + (Application.Current as App).SwFlagList[0].Num + ".txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
-            await Windows.Storage.FileIO.WriteTextAsync(FileCount, (Application.Current as App).SwFlagList.Count.ToString());
-            await Windows.Storage.FileIO.WriteTextAsync(FileFlags, (Application.Current as App).SwFlagList[0].Total + "\n" + (Application.Current as App).SwFlagList[0].Plus + "\n");
+            try
+            {
+                Windows.Storage.StorageFolder StorageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                Windows.Storage.StorageFile FileCount = await StorageFolder.CreateFileAsync("StopWatchFlags\\Count.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+                Windows.Storage.StorageFile FileFlags = await StorageFolder.CreateFileAsync("StopWatchFlags\\Flag" + (Application.Current as App).SwFlagList[0].Num + ".txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+                await Windows.Storage.FileIO.WriteTextAsync(FileCount, (Application.Current as App).SwFlagList.Count.ToString());
+                await Windows.Storage.FileIO.WriteTextAsync(FileFlags, (Application.Current as App).SwFlagList[0].Total + "\n" + (Application.Current as App).SwFlagList[0].Plus + "\n");
+            }
+            catch { }
         }
 
-        private void Page_SizeChanged(object sender = null, SizeChangedEventArgs e = null)
+        int RightMargin = 0;
+        private void SetFontSize()
         {
-            int RightMargin = 0;
-            if ((ActualHeight - 60 - 32 <= 200) || (Application.Current as App).SwFlagList.Count() == 0)
-            {
-                RightMargin = 1;
-                GridTime.VerticalAlignment = VerticalAlignment.Center;
-                if (ActualWidth <= 680)
-                {
-                    ListFlag.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    ListFlag.Margin = new Thickness(0, TimeDisplay.FontSize * 1.2 + ActualHeight / 2 + 120, 0, 0);
-                }
-                else
-                {
-                    ListFlag.HorizontalAlignment = HorizontalAlignment.Right;
-                    ListFlag.Margin = new Thickness(0, 32, 0, 0);
-                }
-                ListFlag.Visibility = Visibility.Visible;
-                FlagSeparator.Visibility = Visibility.Collapsed;
-            }
-            else if (ActualWidth <= 680)
-            {
-                RightMargin = 0;
-                GridTime.VerticalAlignment = VerticalAlignment.Top;
-                ListFlag.Margin = new Thickness(0, TextMinute.FontSize + 60, 0, 60);
-                ListFlag.HorizontalAlignment = HorizontalAlignment.Stretch;
-                ListFlag.VerticalAlignment = VerticalAlignment.Stretch;
-                ListFlag.Width = 300;
-                ListFlag.Visibility = Visibility.Visible;
-                FlagSeparator.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                RightMargin = 280;
-                GridTime.VerticalAlignment = VerticalAlignment.Center;
-                ListFlag.Margin = new Thickness(0, 32, 0, 0);
-                if ((ActualWidth - 280) < (ActualWidth / 2 + 48 + 12 + 24))
-                    ListFlag.Margin = new Thickness(0, 32, 0, 60);
-                ListFlag.Width = 280;
-                ListFlag.HorizontalAlignment = HorizontalAlignment.Right;
-                ListFlag.VerticalAlignment = VerticalAlignment.Stretch;
-                ListFlag.Visibility = Visibility.Visible;
-                FlagSeparator.Visibility = Visibility.Visible;
-            }
-
             if (ActualWidth - RightMargin > 0 && ActualHeight - 60 - 32 > 0)
             {
                 if ((ActualHeight - 60 - 32) / 1.5 > (ActualWidth - RightMargin) / 7)
@@ -353,7 +299,7 @@ namespace Clock
                     TimeDisplay.FontSize = 1;
                 }
             }
-            if (ActualWidth <= 880 || (ActualHeight - 60 - 32) <= 100)
+            if (ActualWidth - RightMargin <= 600 || (ActualHeight - 60 - 32) <= 100)
             {
                 TextH1.FontSize = TextH2.FontSize = TextH3.FontSize = 20;
                 TextH1.Margin = new Thickness(5, 0, 10, TimeDisplay.FontSize / 2.0 - 10);
@@ -375,51 +321,62 @@ namespace Clock
             GridMinute.Height = TimeDisplay.FontSize * 1.2;
             GridSecond.Height = TimeDisplay.FontSize * 1.2;
             GridMSecond.Height = TimeDisplay.FontSize * 1.2;
-            if (RightMargin == 280)
-            {
-                GridTime.Margin = new Thickness(0, 32 + TimeDisplay.FontSize / 10.0, 280, 60);
-            }
-            else if (RightMargin == 0)
-            {
-                GridTime.Margin = new Thickness(0, 48, RightMargin, 12);
-            }
-            else if (RightMargin == 1)
-            {
-                GridTime.Margin = new Thickness(0, 32 + TimeDisplay.FontSize / 10.0, 0, 60);
-            }
+        }
 
-            if ((ActualHeight - 60 - 32 <= 200) || (Application.Current as App).SwFlagList.Count() == 0)
+        private void Page_SizeChanged(object sender = null, SizeChangedEventArgs e = null)
+        {
+            if((Application.Current as App).SwFlagList.Count() == 0)
             {
-                RightMargin = 1;
+                RightMargin = 0;
                 GridTime.VerticalAlignment = VerticalAlignment.Center;
-                ListFlag.HorizontalAlignment = HorizontalAlignment.Stretch;
+                ListFlag.Visibility = Visibility.Visible;
+                FlagSeparator.Visibility = Visibility.Collapsed;
                 if (ActualWidth <= 680)
                 {
+                    ListFlag.Width = 300;
+                    ListFlag.Margin = new Thickness(0, TimeDisplay.FontSize * 1.2 + ActualHeight / 2, 0, 0);
                     ListFlag.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    ListFlag.Margin = new Thickness(0, TimeDisplay.FontSize * 1.2 + ActualHeight / 2 + 120, 0, 0);
                 }
                 else
                 {
-                    ListFlag.HorizontalAlignment = HorizontalAlignment.Right;
+                    ListFlag.Width = 280;
                     ListFlag.Margin = new Thickness(0, 32, 0, 0);
+                    ListFlag.HorizontalAlignment = HorizontalAlignment.Right;
                 }
-                ListFlag.Visibility = Visibility.Visible;
-                FlagSeparator.Visibility = Visibility.Collapsed;
+                SetFontSize();
+                GridTime.Margin = new Thickness(0, 48, 0, 12);
             }
-            else if (ActualWidth <= 680)
+            else if (ActualHeight - 60 - 32 <= 200 && ActualWidth <= 680)
             {
                 RightMargin = 0;
+                ListFlag.Visibility = Visibility.Visible;
+                FlagSeparator.Visibility = Visibility.Collapsed;
+                GridTime.VerticalAlignment = VerticalAlignment.Center;
+                ListFlag.Margin = new Thickness(0, ActualHeight, 0, 60);
+                ListFlag.HorizontalAlignment = HorizontalAlignment.Stretch;
+                ListFlag.VerticalAlignment = VerticalAlignment.Stretch;
+                ListFlag.Width = 300;
+                SetFontSize();
+                GridTime.Margin = new Thickness(0, 32 + TimeDisplay.FontSize / 10.0, 0, 60);
+            }
+            else if(ActualWidth <= 680)
+            {
+                RightMargin = 0;
+                SetFontSize();
+                ListFlag.Visibility = Visibility.Visible;
+                FlagSeparator.Visibility = Visibility.Collapsed;
                 GridTime.VerticalAlignment = VerticalAlignment.Top;
                 ListFlag.Margin = new Thickness(0, TextMinute.FontSize + 60, 0, 60);
                 ListFlag.HorizontalAlignment = HorizontalAlignment.Stretch;
                 ListFlag.VerticalAlignment = VerticalAlignment.Stretch;
                 ListFlag.Width = 300;
-                ListFlag.Visibility = Visibility.Visible;
-                FlagSeparator.Visibility = Visibility.Collapsed;
+                GridTime.Margin = new Thickness(0, 48, 0, 12);
             }
             else
             {
                 RightMargin = 280;
+                ListFlag.Visibility = Visibility.Visible;
+                FlagSeparator.Visibility = Visibility.Visible;
                 GridTime.VerticalAlignment = VerticalAlignment.Center;
                 ListFlag.Margin = new Thickness(0, 32, 0, 0);
                 if ((ActualWidth - 280) < (ActualWidth / 2 + 48 + 12 + 24))
@@ -427,8 +384,8 @@ namespace Clock
                 ListFlag.Width = 280;
                 ListFlag.HorizontalAlignment = HorizontalAlignment.Right;
                 ListFlag.VerticalAlignment = VerticalAlignment.Stretch;
-                ListFlag.Visibility = Visibility.Visible;
-                FlagSeparator.Visibility = Visibility.Visible;
+                SetFontSize();
+                GridTime.Margin = new Thickness(0, 32 + TimeDisplay.FontSize / 10.0, 280, 60);
             }
         }
     }
